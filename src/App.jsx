97bf2566d7ed5bd4c5670bef9daf1679240057df
Deva-1209 <devapd2104@gmail.com>
2025-07-24 19:55:15 +0530

    user cards demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
-import Welcome from './components/welcome';
-import './css/App.css'; // Optional: if you're using global styles
+import React from 'react';
+import UserCard from './components/usercard';
+import './css/App.css';
+
+const users = [
+  {
+    name: 'Devadharshini',
+    email: 'deva1209@gmail.com',
+    profilePic: 'https://api.dicebear.com/7.x/bottts/svg?seed=robotkid',
+  },
+  {
+    name: 'Ravi',
+    email: 'ravi@example.com',
+    profilePic: 'https://i.pravatar.cc/150?img=33',
+  },
+  {
+    name: 'nandy',
+    email: 'nandy@example.com',
+    profilePic: 'https://i.pravatar.cc/150?img=5',
+  },
+];
 
 const App = () => {
   return (
-    <div>
-      <Welcome />
+    <div className="app-container multiple">
+      {users.map((user, index) => (
+        <UserCard
+          key={index}
+          name={user.name}
+          email={user.email}
+          profilePic={user.profilePic}
+        />
+      ))}
     </div>
   );
 };
 
 export default App;
+
